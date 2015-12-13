Pinky-Inky-and-Clyde/proprietary_vendor_htc

dlx: update adreno from qdevenet 30 May 14 KOT49H
Change-Id: I085684bc806e75bbc69822c2e8c1e86c64d2cd67

dlx/dlx-vendor-blobs.mk
@@ -170,6 +170,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/dlx/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
   vendor/htc/dlx/proprietary/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
   vendor/htc/dlx/proprietary/vendor/lib/libllvm-qcom.so:/system/vendor/lib/libllvm-qcom.so \
+  vendor/htc/dlx/proprietary/vendor/lib/liblOpenCL.so:/system/vendor/lib/libOpenCL.so \
   vendor/htc/dlx/proprietary/vendor/lib/librs_adreno_sha1.so:/system/vendor/lib/librs_adreno_sha1.so \
   vendor/htc/dlx/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
   vendor/htc/dlx/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \

wrapped/vendor_htc

dlx/dlx-vendor-blobs.mk
@@ -170,6 +170,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/dlx/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
   vendor/htc/dlx/proprietary/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
   vendor/htc/dlx/proprietary/vendor/lib/libllvm-qcom.so:/system/vendor/lib/libllvm-qcom.so \
+  vendor/htc/dlx/proprietary/vendor/lib/libOpenCL.so:/system/vendor/lib/libOpenCL.so \
   vendor/htc/dlx/proprietary/vendor/lib/librs_adreno_sha1.so:/system/vendor/lib/librs_adreno_sha1.so \
   vendor/htc/dlx/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
   vendor/htc/dlx/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \